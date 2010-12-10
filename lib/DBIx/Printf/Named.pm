38292c51b22340da0ec0afb9b91ac499bd02c031
Masahiro Nagano <kazeburo@gmail.com>
2010-12-10 12:13:12 +0900

    initial commit

---
+++ lib/DBIx/Printf/Named.pm
@@ -0,0 +1,32 @@
+package DBIx::Printf::Named;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Printf::Named -
+
+=head1 SYNOPSIS
+
+  use DBIx::Printf::Named;
+
+=head1 DESCRIPTION
+
+DBIx::Printf::Named is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
